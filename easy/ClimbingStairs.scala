6e03e392d54b92572930745efe8f0395a6c4d654
IAN NAM <ian.nam.kr@gmail.com>
2019-10-03 13:38:26 +0900

    Create ClimbingStairs.scala

---
+++ easy/ClimbingStairs.scala
@@ -0,0 +1,18 @@
+package sample
+
+object climbing_stairs {
+  def climbStairs(n: Int): Int = {
+    val cases = new Array[Int](n + 2)
+    cases(0) = 1
+    cases(1) = 1
+
+    for(step <- 2 to n) {
+      cases(step) = cases(step - 2) + cases(step - 1)
+    }
+    cases(n)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(climbStairs(10))
+  }
+}
