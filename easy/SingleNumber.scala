4ed84c12b149c084c0cc1f0c5db9bb46d51a4e20
IAN NAM <ian.nam.kr@gmail.com>
2019-10-03 14:00:17 +0900

    Create SingleNumber.scala

---
+++ easy/SingleNumber.scala
@@ -0,0 +1,10 @@
+package sample
+
+object SingleNumber {
+  def singleNumber(nums: Array[Int]): Int = nums.reduceLeft(_ ^ _)
+
+  def main(args: Array[String]): Unit = {
+    val input: Array[Int] = Array(4,1,2,1,2,5)
+    print(singleNumber(input))
+  }
+}
