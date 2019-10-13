21ae28cc771cbca74feed853fb20e55dbd483a91
IAN NAM <ian.nam.kr@gmail.com>
2019-10-13 20:53:28 +0900

    Create PowerOfThree.scala

---
+++ easy/PowerOfThree.scala
@@ -0,0 +1,23 @@
+package leetcode
+
+object PowerOfThree {
+  // timeout
+//  def isPowerOfThree(n: Int): Boolean = {
+//    if(n == 1) true
+//    else n % 3 == 0 && isPowerOfThree(n/3)
+//  }
+
+  // timeout
+//  def isPowerOfThree(n: Int): Boolean = {
+//    var tmp = n
+//    while(tmp % 3 == 0)
+//      tmp /= 3
+//    tmp == 1
+//  }
+
+  def isPowerOfThree(n: Int): Boolean = (Math.log10(n) / Math.log10(3)) % 1 == 0
+
+  def main(args: Array[String]): Unit = {
+    print(isPowerOfThree(27))
+  }
+}
